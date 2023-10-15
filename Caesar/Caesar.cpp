1c0dd9d407b3a2264c3d1cc915ddb13343734292
Katherine Shashkina <kshashkina@kse.org.ua>
2023-10-15 22:14:14 +0300

    added one more project for creation of DLL

---
+++ Caesar/Caesar.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+extern "C" {
+__declspec(dllexport) string encrypt(string rawText, int key) {
+    string encryptedText;
+
+    for (char ch : rawText) {
+        if (isalpha(ch)) {
+            char base = islower(ch) ? 'a' : 'A';
+            encryptedText += char(((ch - base + key) % 26) + base);
+        } else {
+            encryptedText += ch;  // Keep non-alphabetic characters unchanged
+        }
+    }
+    return encryptedText;
+}
+
+__declspec(dllexport) string decrypt(string encryptedText, int key) {
+    string decryptedText;
+
+    for (char ch : encryptedText) {
+        if (isalpha(ch)) {
+            char base = islower(ch) ? 'a' : 'A';
+            decryptedText += char(((ch - base - key + 26) % 26) + base);
+        } else {
+            decryptedText += ch;  // Keep non-alphabetic characters unchanged
+        }
+    }
+    return decryptedText;
+}
+}
