1e9fe3bcb740fba4d2f186269f8de7445d172f3f
Katherine Shashkina <kshashkina@kse.org.ua>
2023-10-15 20:44:14 +0300

    added Decrypt() function and implemented main logic

--- main.cpp
+++ main.cpp
@@ -17,7 +17,35 @@ string encrypt(string rawText, int key) {
     return encryptedText;
 }
 
+string decrypt(string encryptedText, int key) {
+    string decryptedText;
+
+    for (char ch : encryptedText) {
+        if (isalpha(ch)) {
+            char base = islower(ch) ? 'a' : 'A';
+            decryptedText += char(((ch - base - key + 26) % 26) + base);
+        } else {
+            decryptedText += ch;  // Keep non-alphabetic characters unchanged
+        }
+    }
+
+    return decryptedText;
+}
 
 int main() {
+    string rawText;
+    int key;
+    cout<< "Write your text"<< endl;
+    cin >> rawText;
+    cout << "Write your key"<<endl;
+    cin>>key;
+
+    string encryptedText = encrypt(rawText, key);
+    string decryptedText = decrypt(encryptedText, key);
+
+    cout << "Original Text: " << rawText << endl;
+    cout << "Encrypted Text: " << encryptedText << endl;
+    cout << "Decrypted Text: " << decryptedText << endl;
+
     return 0;
 }
